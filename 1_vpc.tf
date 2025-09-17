21927238d59df6cd92e16bc43200145d7f4a78fb
schorlastica <schorlasticau@gmail.com>
2025-09-17 21:20:08 +0100

    I just changed the subnet network to true

--- 1_vpc.tf
+++ 1_vpc.tf
@@ -3,6 +3,6 @@
 resource "google_compute_network" "vpc_network" {
   project                 = "host-project-419903"
   name                    = "pci-dss-vpc"
-  auto_create_subnetworks = false
+  auto_create_subnetworks = true
   mtu                     = 1460
 }
